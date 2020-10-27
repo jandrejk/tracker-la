b7e4265c56a4fa942874118a3ecf157088fcf38a
Janik Walter Andrejkovic <jandrejkovic@gmx.net>
2020-10-27 09:26:07 +0100

    adding files for mode identification

---
+++ CalibTrees.py
@@ -0,0 +1,32 @@
+CalibTrees = [
+    {
+        "name" : "2015_MuOpen",
+        "path" : "/eos/cms/store/group/dpg_tracker_strip/comm_tracker/Strip/Calibration/calibrationtree/Cosmics15Legacy/v1/HLT_L1MuOpen",
+        "tag"  : "Cosmics15Legacy/v1/HLT_L1MuOpen"
+    },
+    {
+        "name" : "",
+        "path" : "/eos/cms/store/group/dpg_tracker_strip/comm_tracker/Strip/Calibration/calibrationtree/Cosmics16Legacy/v1/HLT_L1MuOpen",
+        "tag"  : "Cosmics16Legacy/v1/HLT_L1MuOpen"
+    },
+    {
+        "name" : "",
+        "path" : "/eos/cms/store/group/dpg_tracker_strip/comm_tracker/Strip/Calibration/calibrationtree/Cosmics17Legacy/v1/HLT_L1MuCosmics",
+        "tag"  : "Cosmics17Legacy/v1/HLT_L1MuCosmics"
+    },
+    {
+        "name" : "",
+        "path" : "/eos/cms/store/group/dpg_tracker_strip/comm_tracker/Strip/Calibration/calibrationtree/Cosmics18Legacy/v1/HLT_L1MuCosmics",
+        "tag"  : "Cosmics18Legacy/v1/HLT_L1MuCosmics"
+    },
+    {
+        "name" : "",
+        "path" : "/eos/cms/store/group/dpg_tracker_strip/comm_tracker/Strip/Calibration/calibrationtree/Cosmics17Legacy/v1/HLT_L1MuOpen",
+        "tag"  : "Cosmics17Legacy/v1/HLT_L1MuOpen"
+    },
+    {
+        "name" : "",
+        "path" : "/eos/cms/store/group/dpg_tracker_strip/comm_tracker/Strip/Calibration/calibrationtree/Cosmics18Legacy/v1/HLT_L1MuOpen",
+        "tag"  : "Cosmics18Legacy/v1/HLT_L1MuOpen"
+    }
+]
